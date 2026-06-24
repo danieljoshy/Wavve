afff3a15210419ee2b3dda70a99eac66b98cf51a
Daniel Joshy <danieljoshyvymelil@gamil.com>
2026-06-25 00:33:53 +0530

    created index and user model

---
+++ backend/models/user.js
@@ -0,0 +1,52 @@
+const mongoose = require('mongoose')
+const bcrypt = require('bcryptjs')
+
+const userSchema = new mongoose.Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+            trim: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            trim: true,
+            lowercase: true,
+        },
+        password: {
+            type: String,
+            required: true,
+            minlength: 6,
+        },
+        avatar: {
+            type: String,
+            default: '',
+        },
+        isOnline: {
+            type: Boolean,
+            default: false,
+        },
+        isAdmin: {
+            type: Boolean,
+            default: false,
+        },
+    },
+    { timestamps: true }
+)
+
+// Hash password before saving
+userSchema.pre('save', async function (next) {
+    if (!this.isModified('password')) return next()
+    this.password = await bcrypt.hash(this.password, 10)
+    next()
+})
+
+// Compare password on login
+userSchema.methods.matchPassword = async function (enteredPassword) {
+    return await bcrypt.compare(enteredPassword, this.password)
+}
+
+module.exports = mongoose.model('User', userSchema)
